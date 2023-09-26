6d0fecf9d9680f8c1b66754b2d2432b82188b228
Matthijs Brouwer <matthijs@eight.nl>
2023-09-26 10:26:38 +0200

    simple props

--- src/components/testComponent.tsx
+++ src/components/testComponent.tsx
@@ -1,7 +1,13 @@
-export const TestComponent = () => {
+export interface TestComponentProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  icon?: string;
+}
+
+export const TestComponent = ({ children, icon }: TestComponentProps) => {
   return (
     <div>
-      <h1>Test Component</h1>
+      <h1>{children}</h1>
+      <p>{icon}</p>
     </div>
   );
 };
