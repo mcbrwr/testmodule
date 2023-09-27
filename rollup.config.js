d38484de360460e339b313f7613e221322490704
Matthijs Brouwer <matthijs@eight.nl>
2023-09-27 17:11:19 +0200

    workx

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,25 @@
 // Contents of the file /rollup.config.js
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import pkg from "./package.json";
 const config = [
   {
+    input: "src/index.ts",
     output: {
       inlineDynamicImports: true,
       dir: "./dist",
+      format: "es",
     },
-    input: "./src/index.ts",
+    external: [...Object.keys(pkg.dependencies || {}), "react/jsx-runtime"],
     plugins: [
       typescript({
         typescript: require("typescript"),
       }),
       postcss({
         plugins: [],
+        modules: true,
         sourceMap: true,
-        extract: true,
+        extract: false,
         minimize: true,
       }),
     ],
